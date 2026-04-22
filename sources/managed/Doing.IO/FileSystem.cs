289744ff407d822ce3996d6224eb8ed9481ac9f2
godot <me@kawayi.moe>
2026-04-22 17:30:05 +0800

    update

---
+++ sources/managed/Doing.IO/FileSystem.cs
@@ -0,0 +1,9 @@
+// Copyright (c) 2026 MoeGodot<me@kawayi.moe>.
+// Licensed under the GNU Affero General Public License v3-or-later license.
+
+namespace Doing.IO;
+
+public class FileSystem
+{
+    
+}
